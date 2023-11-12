8b7930f6f7e72107493cd555572cbbe529f2d428
iva84 <iva.bazo.7@gmail.com>
2023-11-12 23:36:11 +0100

    navigation

---
+++ components/Navigation.tsx
@@ -0,0 +1,29 @@
+import { getSession } from "@auth0/nextjs-auth0";
+
+import styles from "./Navigation.module.css";
+
+export default async function Navigation() {
+  const session = await getSession();
+
+  return (
+    <nav className={styles["nav"]}>
+      <a className={styles["nav-btn"]} href="/">
+        Home
+      </a>
+      {session ? (
+        <div className={styles["nav-loggedin-container"]}>
+          <a className={styles["nav-btn"]} href="/blogs">
+            Your blogs
+          </a>
+          <a className={styles["nav-btn"]} href="/api/auth/logout">
+            Logout
+          </a>
+        </div>
+      ) : (
+        <a className={styles["nav-btn"]} href="/api/auth/login">
+          Login
+        </a>
+      )}
+    </nav>
+  );
+}
